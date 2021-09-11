abf771d29d785d8198e3e419ad13aaac5f71b862
chithmi <chithmi99suhara@gmail.com>
2021-09-11 21:32:39 +0530

    deleted ITP folder

--- ITP/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UploadRouter from './router/UploadRouter';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.scss';
-
-ReactDOM.render(<UploadRouter />, document.getElementById('root'));
-
